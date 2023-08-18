f6d84e76c1e08e08f11c502439e3ad0da374ad77
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2023-08-18 21:07:21 +0530

    added increment amount

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,20 +1,31 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement, reset } from "./counterSlice";
 
 const Counter = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
+  const [amount, setAmount] = useState(1);
+
+  const value = Number(amount) || 0;
+
+  const resetAll = () => {
+    setAmount(1);
+    dispatch(reset());
+  }
 
   return (
     <div className="card">
       <div>{count}</div>
       <div>
-        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => resetAll()}>Reset</button>
       </div>
       <div>
-        <button onClick={() => dispatch(increment())}> + </button>
-        <button onClick={() => dispatch(decrement())}> - </button>
+        <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
+      </div>
+      <div>
+        <button onClick={() => dispatch(increment(value))}> + </button>
+        <button onClick={() => dispatch(decrement(value))}> - </button>
       </div>
     </div>
   )
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -8,11 +8,12 @@ export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment: (state) => {
-      state.count += 1;
+    increment: (state, action) => {
+      console.log(action);
+      state.count += action.payload;
     },
-    decrement: (state) => {
-      state.count -= 1;
+    decrement: (state, action) => {
+      state.count -= action.payload;
     },
     reset: (state) => {
       state.count = 0;
